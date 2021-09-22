2a67639c13df2f51123782710fa017e94b83d1c1
Your Name <you@example.com>
2021-09-22 15:19:08 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	student.change();
+    student student1 = new student(111, "Hoang");
+    student student2 = new student(222, "Khanh");
+    student student3 = new student(333, "Nam");
+        student1.display();
+        student2.display();
+        student3.display();
+
+    }
+}
